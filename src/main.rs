1c25b2fdb6b41fadbacfbefacb2f3e956f82d997
Zach <zach@basebridge.com>
2019-12-24 10:55:06 -0400

    added blank game window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use tcod::colors::*;
+use tcod::console::*;
+
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+
+const LIMIT_FPS: i32 = 20;
+
+struct Tcod {
+    root: Root,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let root = Root::initializer()
+        .font("arial10x10.png", FontLayout::Tcod)
+        .font_type(FontType::Greyscale)
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("Rust/libtcod tutorial")
+        .init();
+
+    let mut tcod = Tcod { root };
+    tcod::system::set_fps(LIMIT_FPS);
+
+    while !tcod.root.window_closed() {
+        tcod.root.set_default_foreground(WHITE);
+        tcod.root.clear();
+        tcod.root.put_char(1, 1, '@', BackgroundFlag::None);
+        tcod.root.flush();
+        tcod.root.wait_for_keypress(true);
+    }
 }
