21d7a02755e0fe9390bae937ddcf206adfc2d125
Zach <zach@basebridge.com>
2019-12-30 10:38:45 -0400

    added map and wall interaction

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ use tcod::console::*;
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 45;
+const COLOR_DARK_GROUND: Color = Color { r:50, g:50, b:150 };
+const COLOR_DARK_WALL: Color = Color { r:0, g:0, b:100 };
+
 const LIMIT_FPS: i32 = 20;
 
 struct Tcod {
@@ -19,7 +24,7 @@ fn main() {
         .title("Rust/libtcod tutorial")
         .init();
 
-    let con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
+    let con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
     let mut tcod = Tcod { root, con };
     tcod::system::set_fps(LIMIT_FPS);
 
@@ -27,44 +32,33 @@ fn main() {
     let npc = Object::new(SCREEN_WIDTH/2-5, SCREEN_HEIGHT/2, '@', YELLOW);
     let mut objects = [player, npc];
 
+    let game = Game { map: generate_map(), };
     while !tcod.root.window_closed() {
         tcod.con.clear();
 
-        for object in &objects {
-            object.draw(&mut tcod.con);
-        }
-
-        blit(
-            &tcod.con,
-            (0,0),
-            (SCREEN_WIDTH, SCREEN_HEIGHT),
-            &mut tcod.root,
-            (0,0),
-            1.0,
-            1.0,
-        );
+        render_all(&mut tcod, &game, &objects);
         tcod.root.flush();
         tcod.root.wait_for_keypress(true);
         
         let player = &mut objects[0];
-        let exit = handle_keys(&mut tcod, player);
+        let exit = handle_keys(&mut tcod, &game, player);
         if exit {
             break;
         }
     }
 }
 
-fn handle_keys(tcod: &mut Tcod, player: &mut Object) -> bool {
+fn handle_keys(tcod: &mut Tcod, game: &Game, player: &mut Object) -> bool {
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
 
     let key = tcod.root.wait_for_keypress(true); 
     match key {
         // Movement Keys
-        Key { code: Up, .. } => player.move_by(0,-1),
-        Key { code: Down, .. } => player.move_by(0,1),
-        Key { code: Left, .. } => player.move_by(-1,0),
-        Key { code: Right, .. } => player.move_by(1,0),
+        Key { code: Up, .. } => player.move_by(0,-1, game),
+        Key { code: Down, .. } => player.move_by(0,1, game),
+        Key { code: Left, .. } => player.move_by(-1,0, game),
+        Key { code: Right, .. } => player.move_by(1,0, game),
 
         Key { code: Enter, alt: true, .. } =>  { 
             let fullscreen = tcod.root.is_fullscreen();
@@ -78,6 +72,42 @@ fn handle_keys(tcod: &mut Tcod, player: &mut Object) -> bool {
     false
 }
 
+fn generate_map() -> Map {
+    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+    map[30][22] = Tile::wall();
+    map[50][22] = Tile::wall();
+    map
+}
+
+fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
+
+        for object in objects {
+            object.draw(&mut tcod.con);
+        }
+
+        for y in 0..MAP_HEIGHT {
+            for x in 0..MAP_WIDTH {
+                let wall = game.map[x as usize][y as usize].block_sight;
+                if wall {
+                    tcod.con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
+                } else {
+                    tcod.con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+                }
+            }
+        }
+
+        blit(
+            &tcod.con,
+            (0,0),
+            (MAP_WIDTH, MAP_HEIGHT),
+            &mut tcod.root,
+            (0,0),
+            1.0,
+            1.0,
+        );
+}
+
+#[derive(Debug)]
 struct Object {
     x: i32,
     y: i32,
@@ -89,13 +119,42 @@ impl Object {
         Object { x, y, char, color }
     }
 
-    pub fn move_by(&mut self, dx: i32, dy: i32) {
-        self.x += dx;
-        self.y += dy;
+    pub fn move_by(&mut self, dx: i32, dy: i32, game: &Game) {
+        if !game.map[(self.x + dx) as usize][(self.y + dy) as usize].blocked {
+            self.x += dx;
+            self.y += dy;
+        }
     }
 
     pub fn draw(&self, con: &mut dyn Console) {
         con.set_default_foreground(self.color);
         con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
     }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    blocked: bool,
+    block_sight: bool,
+}
+impl Tile {
+    pub fn empty() -> Self {
+        Tile {
+            blocked: false,
+            block_sight: false,
+        }
+    }
+
+    pub fn wall() -> Self {
+        Tile {
+            blocked: true,
+            block_sight: true,
+        }
+    }
+}
+
+type Map = Vec<Vec<Tile>>;
+
+struct Game {
+    map: Map,
 }
