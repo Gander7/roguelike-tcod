2f43dbf44593074d97a8c09f9974a64cf19dd372
Zach <zach@basebridge.com>
2019-12-30 10:04:43 -0400

    added Object

--- src/main.rs
+++ src/main.rs
@@ -23,13 +23,17 @@ fn main() {
     let mut tcod = Tcod { root, con };
     tcod::system::set_fps(LIMIT_FPS);
 
-    let mut player_x = SCREEN_WIDTH/2;
-    let mut player_y = SCREEN_HEIGHT/2;
+    let player = Object::new(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', WHITE);
+    let npc = Object::new(SCREEN_WIDTH/2-5, SCREEN_HEIGHT/2, '@', YELLOW);
+    let mut objects = [player, npc];
 
     while !tcod.root.window_closed() {
-        tcod.con.set_default_foreground(WHITE);
         tcod.con.clear();
-        tcod.con.put_char(player_x, player_y, '@', BackgroundFlag::None);
+
+        for object in &objects {
+            object.draw(&mut tcod.con);
+        }
+
         blit(
             &tcod.con,
             (0,0),
@@ -41,25 +45,26 @@ fn main() {
         );
         tcod.root.flush();
         tcod.root.wait_for_keypress(true);
-
-        let exit = handle_keys(&mut tcod, &mut player_x, &mut player_y);
+        
+        let player = &mut objects[0];
+        let exit = handle_keys(&mut tcod, player);
         if exit {
             break;
         }
     }
 }
 
-fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool {
+fn handle_keys(tcod: &mut Tcod, player: &mut Object) -> bool {
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
 
     let key = tcod.root.wait_for_keypress(true); 
     match key {
         // Movement Keys
-        Key { code: Up, .. } => *player_y -= 1,
-        Key { code: Down, .. } => *player_y += 1,
-        Key { code: Left, .. } => *player_x -= 1,
-        Key { code: Right, .. } => * player_x += 1,
+        Key { code: Up, .. } => player.move_by(0,-1),
+        Key { code: Down, .. } => player.move_by(0,1),
+        Key { code: Left, .. } => player.move_by(-1,0),
+        Key { code: Right, .. } => player.move_by(1,0),
 
         Key { code: Enter, alt: true, .. } =>  { 
             let fullscreen = tcod.root.is_fullscreen();
@@ -71,4 +76,26 @@ fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool 
     }
 
     false
+}
+
+struct Object {
+    x: i32,
+    y: i32,
+    char: char,
+    color: Color,
+}
+impl Object {
+    pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
+        Object { x, y, char, color }
+    }
+
+    pub fn move_by(&mut self, dx: i32, dy: i32) {
+        self.x += dx;
+        self.y += dy;
+    }
+
+    pub fn draw(&self, con: &mut dyn Console) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+    }
 }
