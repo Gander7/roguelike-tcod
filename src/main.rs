d8a3e727c67c1d2ba590bc4cead2c5e63109e782
Zach <zach@basebridge.com>
2019-12-30 12:24:56 -0400

    added fov

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use tcod::colors::*;
 use tcod::console::*;
+use tcod::map::{FovAlgorithm, Map as FovMap};
 use rand::Rng;
 
 const SCREEN_WIDTH: i32 = 80;
@@ -8,7 +9,9 @@ const SCREEN_HEIGHT: i32 = 50;
 const MAP_WIDTH: i32 = 80;
 const MAP_HEIGHT: i32 = 45;
 const COLOR_DARK_GROUND: Color = Color { r:50, g:50, b:150 };
+const COLOR_LIGHT_GROUND: Color = Color { r:200, g:180, b:50 };
 const COLOR_DARK_WALL: Color = Color { r:0, g:0, b:100 };
+const COLOR_LIGHT_WALL: Color = Color { r:130, g:110, b:50 };
 
 const ROOM_MAX_SIZE: i32 = 10;
 const ROOM_MIN_SIZE: i32 = 6;
@@ -16,9 +19,14 @@ const MAX_ROOMS: i32 = 30;
 
 const LIMIT_FPS: i32 = 20;
 
+const FOV_ALGOR: FovAlgorithm = FovAlgorithm::Basic;
+const FOV_LIGHT_WALLS: bool = true;
+const TORCH_RADIUS: i32 = 10;
+
 struct Tcod {
     root: Root,
     con: Offscreen,
+    fov: FovMap,
 }
 
 fn main() {
@@ -29,8 +37,11 @@ fn main() {
         .title("Rust/libtcod tutorial")
         .init();
 
-    let con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
-    let mut tcod = Tcod { root, con };
+    let mut tcod = Tcod { 
+        root, 
+        con: Offscreen::new(MAP_WIDTH, MAP_HEIGHT), 
+        fov: FovMap::new(MAP_WIDTH, MAP_HEIGHT) 
+    };
     tcod::system::set_fps(LIMIT_FPS);
 
     let player = Object::new(0, 0, '@', WHITE);
@@ -38,14 +49,29 @@ fn main() {
     let mut objects = [player, npc];
 
     let game = Game { map: generate_map(&mut objects[0]), };
+
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            tcod.fov.set(
+                x,
+                y,
+                !game.map[x as usize][y as usize].block_sight,
+                !game.map[x as usize][y as usize].blocked,
+            );
+        }
+    }
+    let mut previous_player_position = (-1, -1);
+
     while !tcod.root.window_closed() {
         tcod.con.clear();
 
-        render_all(&mut tcod, &game, &objects);
+        let fov_recompute = previous_player_position != (objects[0].x, objects[0].y);
+        render_all(&mut tcod, &game, &objects, fov_recompute);
         tcod.root.flush();
         tcod.root.wait_for_keypress(true);
         
         let player = &mut objects[0];
+        previous_player_position = (player.x, player.y);
         let exit = handle_keys(&mut tcod, &game, player);
         if exit {
             break;
@@ -119,32 +145,42 @@ fn generate_map(player: &mut Object) -> Map {
     map
 }
 
-fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
+fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object], fov_recompute: bool) {
 
-        for object in objects {
+    if fov_recompute {
+        let player = &objects[0];
+        tcod.fov.compute_fov(player.x, player.y, TORCH_RADIUS, FOV_LIGHT_WALLS, FOV_ALGOR);
+    }
+
+    for object in objects {
+        if tcod.fov.is_in_fov(object.x, object.y) {
             object.draw(&mut tcod.con);
         }
+    }
 
-        for y in 0..MAP_HEIGHT {
-            for x in 0..MAP_WIDTH {
-                let wall = game.map[x as usize][y as usize].block_sight;
-                if wall {
-                    tcod.con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
-                } else {
-                    tcod.con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
-                }
-            }
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let visible = tcod.fov.is_in_fov(x,y);
+            let wall = game.map[x as usize][y as usize].block_sight;
+            let color = match(visible, wall) {
+                (false, true) => COLOR_DARK_WALL,
+                (false, false) => COLOR_DARK_GROUND,
+                (true, true) => COLOR_LIGHT_WALL,
+                (true, false) => COLOR_LIGHT_GROUND,
+            };
+            tcod.con.set_char_background(x, y, color, BackgroundFlag::Set);
         }
+    }
 
-        blit(
-            &tcod.con,
-            (0,0),
-            (MAP_WIDTH, MAP_HEIGHT),
-            &mut tcod.root,
-            (0,0),
-            1.0,
-            1.0,
-        );
+    blit(
+        &tcod.con,
+        (0,0),
+        (MAP_WIDTH, MAP_HEIGHT),
+        &mut tcod.root,
+        (0,0),
+        1.0,
+        1.0,
+    );
 }
 
 #[derive(Debug)]
